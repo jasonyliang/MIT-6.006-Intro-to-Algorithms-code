76e4365b21f71634ae1bd423b363bd9a1019646d
Jason Liang <jjjasonliang@gmail.com>
2018-03-09 15:25:42 -0800

    switch to min heap

--- 2. Sorting and Trees/heap.py
+++ 2. Sorting and Trees/heap.py
@@ -2,15 +2,19 @@
 Identify a heap: a data structure visualized in a tree with either heap max or heap min property
 
 '''
-
-def is_max_heap(lst):
+import heapq
+x = [0, 1, 3,4,2,1,3]
+heapq.heapify(x)
+def is_min_heap(lst):
     '''
-    identify whether a given list is a max heap
+    identify whether a given list is a min heap
     '''
     heap = True
-    for n in range(int(len(lst)/2)):
-        if lst[2*n] <= lst[n] and lst[2*n+1] <= lst[n]:
+    for n in range(1,int(len(lst)/2)+1):
+        if lst[2*n-1] >= lst[n-1] and lst[2*n-1] >= lst[n-1]:
             heap = True
         else:
             return False
     return heap
+print(x)
+print(is_max_heap(x))
