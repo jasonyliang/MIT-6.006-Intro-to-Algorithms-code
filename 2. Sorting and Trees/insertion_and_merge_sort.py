600beab9668c722a57a70b92047fa2f594a32dce
Jason Liang <jjjasonliang@gmail.com>
2018-03-11 11:18:17 -0700

    refined merge sort
    Shout out to Jennifer (學姊) :smile:

--- 2. Sorting and Trees/insertion_and_merge_sort.py
+++ 2. Sorting and Trees/insertion_and_merge_sort.py
@@ -25,3 +25,55 @@ def mergesort(lst):
 
 
     return lst
+
+def merge_sort(L):
+    if len(L) > 1:
+        q = len(L)//2
+        left = merge_sort(L[:q])
+        right = merge_sort(L[q:])
+        return merge(left,right)
+    return L
+def merge(left,right):
+    A = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            A.append(left[i])
+            i += 1
+        else:
+            A.append(right[j])
+            j += 1
+    A += left[i:] #If two lists are of different length, the while loop stops when the elements in the shorter
+    A += right[j:] #list are all appended. Therefore, we need to append whatever is left in the longer list.
+    return A
+
+def new_merge(lst):
+    if len(lst)>1:
+        cut = int(len(lst)/2)
+        left = new_merge(lst[:cut])
+        right = new_merge(lst[cut:])
+        return new_mergesort(left, right)
+    return lst
+def new_mergesort(left, right):
+    final = []
+    l = 0
+    r = 0
+    status = True
+    while status:
+        if left[l] <= right[r]:
+            final.append(left[l])
+            l += 1
+            if l == len(left):
+                status = False
+
+        else:
+            final.append(right[r])
+            r += 1
+            if r == len(right):
+                status = False
+    final += left[l:]
+    final +=right[r:]
+    return final
+
+print(new_merge(sample1))
